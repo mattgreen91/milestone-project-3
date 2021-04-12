324628a669d3699892fe2d52c08aa678bb085009
mattgreen91 <mattgreen91@outlook.com>
2021-04-12 03:40:21 +0000

    added flask, created run.py with basic flask for each webpage,  and modified all URLs into flask format

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
+@app.route("/store")
+def store():
+    return render_template("store.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
