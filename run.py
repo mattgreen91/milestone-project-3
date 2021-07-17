b461e21329c165416ccb9553f835a0998fcf24c5
mattgreen91 <mattgreen91@outlook.com>
2021-07-17 19:57:13 +0000

    renamed title and created error 500 page

--- run.py
+++ run.py
@@ -175,8 +175,13 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+        debug=False)
