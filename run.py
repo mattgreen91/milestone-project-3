f9b62f0f06c138b460160c0eab64b1037764da41
mattgreen91 <mattgreen91@outlook.com>
2021-07-18 18:07:30 +0000

    updated comments in all html/py files

--- run.py
+++ run.py
@@ -16,11 +16,13 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# error 404 lost page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
+# error 500 internal error page
 @app.errorhandler(500)
 def internal_error(e):
     return render_template('500.html'), 500
@@ -28,13 +30,16 @@ def internal_error(e):
 
 @app.route("/")
 def index():
+    # takes all spotting posts and stores as a list
     posts = list(mongo.db.spotting_post.find())
     return render_template("index.html", posts=posts, page_title="Home")
 
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
+    # takes the search item from the index page
     searchitem = request.form.get("searchitem")
+    # looks for the index of text value in mongodb
     posts = list(mongo.db.spotting_post.find({"$text": {"$search": searchitem}}))
     return render_template("index.html", posts=posts, page_title="Home")
 
@@ -46,10 +51,12 @@ def new_user():
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
+        # alerts user the account exists, and reloads page
         if existing_user:
             flash("This username is already taken")
             return redirect(url_for("new_user"))
 
+        # new user details collected from input fields and stored into database
         new_user = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -95,15 +102,19 @@ def login():
 
 @app.route("/modify_cars")
 def modify_cars():
+    # takes all cars into a list for displaying
     cars = list(mongo.db.car.find().sort("car_make", 1))
+    # this function is only allowed if user account is admin (admin123)
     if session["user"] == "admin123":
         return render_template("modify_cars.html", cars=cars)
     else:
+        # if any other user tries to open page, will redirect to homepage
         return redirect(url_for("index"))
 
 
 @app.route("/new_make", methods=["GET", "POST"])
 def new_make():
+    # adds new car from input field to database
     if request.method == "POST":
         car_make = {
             "car_make": request.form.get("car_make"),
@@ -115,6 +126,7 @@ def new_make():
 
 @app.route("/remove_make/<car_id>")
 def remove_make(car_id):
+    # deletes the car make from the database
     mongo.db.car.delete_one({"_id": ObjectId(car_id)})
     flash("Car Make Has Been Removed")
     return redirect(url_for("modify_cars"))
@@ -123,6 +135,7 @@ def remove_make(car_id):
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
+        # all below fields are taken from input fields and posted to database
         post = {
             "car_make": request.form.get("car_make"),
             "car_model": request.form.get("car_model"),
@@ -142,6 +155,7 @@ def add():
 
 @app.route("/edit/<post_id>", methods=["GET", "POST"])
 def edit(post_id):
+    # modifies car information from form for saving changes
     if request.method == "POST":
         submit = {
             "car_make": request.form.get("car_make"),
@@ -162,6 +176,7 @@ def edit(post_id):
 
 @app.route("/remove/<post_id>")
 def remove(post_id):
+    # deletes spotting post from the database
     mongo.db.spotting_post.delete_one({"_id": ObjectId(post_id)})
     flash("Spotting Post Has Been Removed")
     return redirect(url_for("index"))
@@ -169,14 +184,16 @@ def remove(post_id):
 
 @app.route("/account_settings", methods=["GET", "POST"])
 def account_settings():
-    # take username from session on database
+    # checks if there is an active session, and takes the username
     if session["user"]:
         username = session["user"]
 
+        # checks user is in mongodb
         user = mongo.db.users.find_one(
         {"username": username})
 
         if user:
+            # displays account settings for the user in session
             return render_template("account_settings.html", user=user)
 
     return redirect(url_for("login.html"))
@@ -193,8 +210,11 @@ def logout():
 @app.route("/change_password/<user_id>", methods=["GET", "POST"])
 def change_password(user_id):
     
+    # changes only the password of the user id
     if request.method == "POST":
-        mongo.db.users.update_one({"_id": ObjectId(user_id)},{"$set":{"password": generate_password_hash(request.form.get("password"))}})
+        mongo.db.users.update_one(
+            {"_id": ObjectId(user_id)},{"$set":{"password": generate_password_hash(
+                request.form.get("password"))}})
 
         flash("Password updated successfully")
         return redirect(url_for("account_settings", username=session["user"]))
@@ -204,8 +224,10 @@ def change_password(user_id):
 
 @app.route("/delete_account/<user_id>")
 def delete_account(user_id):
+    # allows current user to be deleted from database
     mongo.db.users.delete_one({"_id": ObjectId(user_id)})
     flash("User Deleted")
+    # removes cookie so user is logged off
     session.pop("user")
     return redirect(url_for("login"))
 
